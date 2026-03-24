f0caef3ce6cd547de9067eaa20cbad732c317c58
Arun Sahadeo <arunjamessahadeo@gmail.com>
2026-03-24 19:22:24 +0000

    Attempted scraping fix

--- scraper/build_snapshot.py
+++ scraper/build_snapshot.py
@@ -4,6 +4,10 @@ import os
 from scraper.rss_feeds import RSS_FEEDS
 from scraper.fetch_rss import fetch_rss_entries
 from scraper.parse_request_page import scrape_request_page
+from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+import requests
+import re
 
 def build_snapshot():
     os.makedirs("snapshots", exist_ok=True)
@@ -16,13 +20,41 @@ def build_snapshot():
         entries = fetch_rss_entries(url)
 
         for e in entries:
-            request_url = e.link
+            request_url = None
+            title = e.title
+            published = e.published
+
+            authority = None
+            submitter = None
+
+            content = str(e.content[0].value).replace('&lt', '<').replace('&gt;', '>').replace('&quot;', '"')
+            m = re.search('<a href="https://www.whatdotheyknow.com/body/[^"]+">', content)
+
+            if m is not None:
+                authority = str(m.group(0)).replace('<a href="https://www.whatdotheyknow.com/body/', '').replace('">', '')
+
+            m = re.search('<a href="https://www.whatdotheyknow.com/user/[^"]+">', content)
+
+            if m is not None:
+                submitter = str(m.group(0)).replace('<a href="https://www.whatdotheyknow.com/user/', '').replace('">', '')
+
+
+            if submitter and authority:
+                r = requests.get("https://www.whatdotheyknow.com/search/requests?query=" + '"' + quote_plus(title) + '"' + "+requested_by:" + submitter + "+requested_from:" + authority)
+                soup = BeautifulSoup(r.text, "html.parser")
+                search_results = soup.select(".foi_results + .results_block > .request_listing")
+
+                if len(search_results) > 0:
+                    for result in search_results:
+                        request_url = result.select_one(".request_left > .head > a").get("href")
+                        if request_url.startswith("/"):
+                            request_url = "https://www.whatdotheyknow.com" + request_url
 
             details = scrape_request_page(request_url)
 
             rows.append({
                 "category": category,
-                "title": e.title,
+                "title": title,
                 "published": e.published,
                 "request_url": request_url,
                 **details
--- scraper/parse_request_page.py
+++ scraper/parse_request_page.py
@@ -2,20 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_request_page(url):
-    r = requests.get(url, timeout=20)
+    r = requests.get(url)
+    print(f"The resolved URL and status code: {r.url}, {r.status_code}")
     soup = BeautifulSoup(r.text, "html.parser")
 
     # Submitter
-    submitter = soup.select_one(".requester a")
+    submitter = soup.select(".request-header__action-bar-container .request-header__subtitle a:first-child")
     submitter = submitter.text.strip() if submitter else None
 
     # Authority
-    authority = soup.select_one(".public_body a")
+    authority = soup.select(".request-header__action-bar-container .request-header__subtitle a:last-child")
     authority = authority.text.strip() if authority else None
 
+    valid_statuses = [
+        'waiting_response',
+        'not_held',
+        'rejected',
+        'partially_successful',
+        'successful',
+        'waiting_clarification',
+        'gone_postal',
+        'internal_review',
+        'error_message',
+        'requires_admin',
+        'user_withdrawn'
+    ]
+
     # Status
-    status = soup.select_one(".request_status")
-    status = status.text.strip() if status else None
+    status = soup.select_one("#request_status").get("class").split('request-status-message--')[1]
+
+    if status not in valid_statuses:
+        print(f"The invalid status: {status}")
+        status = None
 
     # Last message (resolution)
     messages = soup.select(".correspondence")
@@ -26,16 +44,17 @@ def scrape_request_page(url):
 
     # Attachments
     attachments = []
-    for a in soup.select(".attachment a"):
-        href = a.get("href")
-        if href:
-            attachments.append("https://www.whatdotheyknow.com" + href)
+    for attachment in soup.select(".attachment"):
+        attachment_item_title = attachment.select(".attachment__name").text.strip()
+        attachment_item_url = attachment.select_one(".attachment__meta a").get("href")
+
+        if attachment_item_title and attachment_item_title_url:
+            attachments.append({"title": attachment_item_title, "url": "https://www.whatdotheyknow.com" + attachment_item_url})
 
     return {
         "submitter": submitter,
         "authority": authority,
         "status": status,
         "resolution_text": resolution_text,
-        "attachments": ";".join(attachments)
+        "attachments": attachments
     }
-
