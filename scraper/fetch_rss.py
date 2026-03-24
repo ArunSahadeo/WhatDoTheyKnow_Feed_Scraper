0555deaa9da5fc1bd49d5553e9310bab4f77f5c1
Arun Sahadeo <arunjamessahadeo@gmail.com>
2026-03-24 19:55:40 +0000

    Additional logging

--- scraper/fetch_rss.py
+++ scraper/fetch_rss.py
@@ -2,4 +2,8 @@ import feedparser
 
 def fetch_rss_entries(url):
     feed = feedparser.parse(url)
+    print(f"The feed data: {feed.feeddata}")
+    print(f"The feed entries: {feed.entries}")
+    print(f"The feed version: {feed.version}")
+    print(f"The feed namespaces: {feed.namespaces}")
     return feed.entries
