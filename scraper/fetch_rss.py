e3e76017c62016bdb54aa92b17d832f0751225fd
Arun Sahadeo <arunjamessahadeo@gmail.com>
2026-03-24 19:57:36 +0000

    Update property name

--- scraper/fetch_rss.py
+++ scraper/fetch_rss.py
@@ -2,7 +2,7 @@ import feedparser
 
 def fetch_rss_entries(url):
     feed = feedparser.parse(url)
-    print(f"The feed data: {feed.feeddata}")
+    print(f"The feed data: {feed.feed}")
     print(f"The feed entries: {feed.entries}")
     print(f"The feed version: {feed.version}")
     print(f"The feed namespaces: {feed.namespaces}")
