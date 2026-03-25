4ce9189654e4b61247eb6a62dd0fa748696b12ae
Arun Sahadeo <arunjamessahadeo@gmail.com>
2026-03-25 18:58:41 +0000

    Add debugging

--- scraper/fetch_rss.py
+++ scraper/fetch_rss.py
@@ -4,4 +4,10 @@ def fetch_rss_entries(url):
     import requests
     r = requests.get(url)
     feed = feedparser.parse(r.text if r.status_code == 200 else '')
+
+    if r.status_code != 200:
+        import base64
+        content = base64.b64encode(r.text.encode())
+        print(f"The base64-encoded content from {url}: {content}")
+
     return feed.entries
